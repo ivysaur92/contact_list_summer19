87a78e96967395bc88439b0dbf7bfb37f20eace1
Ivanna Brooks <ibrooks@its.nicholls.edu>
2019-05-21 20:51:58 -0600

    end of lecture

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_22_002135) do
+ActiveRecord::Schema.define(version: 2019_05_22_013048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "contacts", force: :cascade do |t|
-    t.string "name"
+    t.string "first_name"
     t.integer "age"
     t.string "phone"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "last_name"
+    t.boolean "friend"
   end
 
+  create_table "notes", force: :cascade do |t|
+    t.text "body"
+    t.bigint "contact_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["contact_id"], name: "index_notes_on_contact_id"
+  end
+
+  add_foreign_key "notes", "contacts"
 end
